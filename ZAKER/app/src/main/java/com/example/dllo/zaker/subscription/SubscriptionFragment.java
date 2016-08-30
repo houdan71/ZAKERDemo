9e3c482a3f3cdef477ff6e311a9c3f912c893af7
dllo <dllo@liuyaping.local>
2016-08-30 16:25:29 +0800

    订阅

--- ZAKER/app/src/main/java/com/example/dllo/zaker/subscription/SubscriptionFragment.java
+++ ZAKER/app/src/main/java/com/example/dllo/zaker/subscription/SubscriptionFragment.java
@@ -1,6 +1,9 @@
 package com.example.dllo.zaker.subscription;
 
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.GridView;
 
 import com.example.dllo.zaker.R;
 import com.example.dllo.zaker.base.BaseFragment;
@@ -9,20 +12,28 @@ import com.example.dllo.zaker.base.BaseFragment;
  * Created by dllo on 16/8/29.
  */
 public class SubscriptionFragment extends BaseFragment {
+     private ViewPager view_pager;
+     private GridView grid_view;
 
 
     @Override
     protected int initLayout() {
+
         return R.layout.fragment_subscription;
     }
 
     @Override
     protected void initView(View view) {
+        View view1 = LayoutInflater.from(mContext).inflate(R.layout.head_subcsription,null);
+        view_pager= (ViewPager) view.findViewById(R.id.view_pager);
+        grid_view = (GridView) view1.findViewById(R.id.grid_view);
+        grid_view.addView(view1);
 
     }
 
     @Override
     protected void initData() {
+
 
     }
 }
